8cbdedb35a1817952f62a3155853580f268dcc35
Aniket15desai <omkarsardesai14@gmail.com>
2020-09-03 13:19:48 +0530

    Changes
    Hi

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 <script>
-			$(document).ready(function(){
-    		$('.menu').click(function(){
-    			$('ul').toggleClass('active')
-    		})
-    	})
+	$(document).ready(function(){
+		$('.menu').click(function(){
+    		$('ul').toggleClass('active')
+		})
+			
+	}
 
-		</script>
+</script>
